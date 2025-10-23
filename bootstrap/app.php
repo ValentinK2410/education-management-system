f0c6f9e023e070664d2571483d7c1a6ab208e09d
Valentink2410 <valentink2410@gmail.com>
2025-10-23 06:09:02 +0400

    Исправлена ошибка middleware 'role' - Target class does not exist
    - Зарегистрирован middleware 'role' в bootstrap/app.php
    - Добавлен алиас для CheckRole middleware
    - Добавлен импорт модели Role в AuthController
    - Исправлена ошибка доступа к административным маршрутам

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Регистрация middleware для проверки ролей
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
