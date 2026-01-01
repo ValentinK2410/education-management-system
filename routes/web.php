f9f24dee495ad152450a175f1f87c0b811ded399
Valentink2410 <valentink2410@gmail.com>
2026-01-01 14:56:43 +0400

    Удалены тестовые маршруты профиля (test-profile и test-profile-edit)

--- routes/web.php
+++ routes/web.php
@@ -165,24 +165,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/profile/edit', [ProfileController::class, 'edit'])->name('admin.profile.edit');
     Route::put('/admin/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
 
-    // Тестовые маршруты профиля (временно без контроллера)
-    Route::get('/admin/test-profile', function () {
-        try {
-            $user = auth()->user();
-            return view('admin.profile.show', compact('user'));
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()]);
-        }
-    })->name('admin.test-profile');
-
-    Route::get('/admin/test-profile-edit', function () {
-        try {
-            $user = auth()->user();
-            return view('admin.profile.edit', compact('user'));
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()]);
-        }
-    })->name('admin.test-profile-edit');
 
     // Тестовый маршрут для проверки сохранения программы
     Route::post('/admin/test-program-update', function (Request $request) {
