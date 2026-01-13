4f0e0fd363da25cc2e2d69b95dcd2139715709b4
Valentink2410 <valentink2410@gmail.com>
2026-01-13 19:38:49 +0400

    Исправление: добавлен отсутствующий маршрут admin.users.show

--- routes/web.php
+++ routes/web.php
@@ -444,6 +444,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('users', [UserController::class, 'index'])->name('users.index');
         Route::get('users/create', [UserController::class, 'create'])->name('users.create');
         Route::post('users', [UserController::class, 'store'])->name('users.store');
+        Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
         Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
         Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
         Route::post('users/bulk-destroy', [UserController::class, 'bulkDestroy'])->name('users.bulk-destroy');
